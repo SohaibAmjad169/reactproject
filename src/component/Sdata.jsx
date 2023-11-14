f299f5901ca35c6f8510fec8aaa20d528e62306b
Sohaib Amjad <103494617+SohaibAmjad169@users.noreply.github.com>
2023-11-14 23:28:34 +0500

    First Commit

---
+++ src/component/Sdata.jsx
@@ -0,0 +1,36 @@
+import web from "../images/web.jpg";
+import and from "../images/and.jpg";
+import dig from "../images/dig.jpg";
+import soft from "../images/soft.jpg";
+import mark from "../images/mark.jpeg";
+import app from "../images/app.jpg";
+
+
+const sData= [
+    {
+        imgsrc: web,
+        title: "Web Development"
+    },
+    {
+        imgsrc: and,
+        title: "Android Development"
+    },
+    {
+        imgsrc: dig,
+        title: "Digital Marketing"
+    },
+    {
+        imgsrc: soft,
+        title: "Software Marketing"
+    },
+    {
+        imgsrc: mark,
+        title: "Marketing"
+    },
+    {
+        imgsrc: app,
+        title: "App Developments"
+    },
+]
+
+export default sData;
